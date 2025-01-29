156ff31ee3ee7b7e84a9f9a8baa612d43c972d9c
abidkhokhar <dev.abidkk@gmail.com>
2025-01-29 20:33:16 +0500

    class1 code

---
+++ Class1/conditions/else.js
@@ -0,0 +1,11 @@
+let name = 'Ali';
+let role = 'Admin';
+
+//  conditoin
+
+if (name == 'Adam'){
+    console.log('Welcome')
+}
+else {
+    console.log('Incorrect username')
+}
---
+++ Class1/conditions/if.js
@@ -0,0 +1,8 @@
+let name = 'Ali';
+let role = 'Admin';
+
+// condition
+if (name == 'Ali'){
+    console.log('Welcome Ali')
+}
+
